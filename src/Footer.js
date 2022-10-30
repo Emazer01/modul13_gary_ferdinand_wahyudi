c76b5e46ec63823008a82cb0f96ecdb0951c6f2c
Emazer01 <garyferdinand15@gmail.com>
2022-10-30 08:44:00 +0700

    Add Component

---
+++ src/Footer.js
@@ -0,0 +1,11 @@
+const Footer = () => {
+    return (
+        <footer>
+            <h3>Gary Ferdinand Wahyudi</h3>
+            <h3>Netlab Bookstore</h3>
+            <h3>320200401008</h3>
+        </footer>
+    )
+ }
+
+ export default Footer
---
+++ src/Title.js
@@ -0,0 +1,7 @@
+ const Title = () => {
+    return (
+        <h1>Netlab Bookstore</h1>
+    )
+ }
+
+ export default Title
